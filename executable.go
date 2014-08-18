3b7a8d93c39c39b4791c68c2fda8846dfbd5f469
Justinas Stankevicius <justinas@justinas.me>
2014-08-18 13:07:49 +0300

    Remove useless TemplateMock type

--- executable.go
+++ executable.go
@@ -1,14 +1,13 @@
 package plate
 
 import (
-    "io"
-    )
+	"io"
+)
 
-type Executable interface{
-    Execute(wr io.Writer, data interface{}) error
-    ExecuteTemplate(wr io.Writer, name string, data interface{}) error
+// Executable is an interface comprised of the metods
+// that html/template and text/template use to render themselves.
+// Thus any *template.Template implements Executable automatically.
+type Executable interface {
+	Execute(wr io.Writer, data interface{}) error
+	ExecuteTemplate(wr io.Writer, name string, data interface{}) error
 }
-
-// Satisfies Executable
-func (t *TemplateMock) Execute(wr io.Writer, data interface{}) error
-func (t *TemplateMock) ExecuteTemplate(wr io.Writer, name string, data interface{}) error
